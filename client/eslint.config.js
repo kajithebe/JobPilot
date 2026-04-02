36845a993b0ef190162557488cf6aae11c86e3eb
Miraj Shrestha <mirajs@metropolia.fi>
2026-04-02 09:16:49 +0300

    feat: scaffold Vite + React 18 with Tailwind, ESLint, Prettier and folder structure

---
+++ client/eslint.config.js
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  { ignores: ['dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      prettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': 'warn',
+      'no-console': 'warn',
+    },
+  },
+];
